fe444984a7747998a0668fdbd7a83cf3fb999448
DONTKILLME <ashishnegi0511@gmail.com>
2022-06-23 15:18:57 +0530

    thread

--- multipleRunner.py
+++ multipleRunner.py
@@ -1,5 +1,6 @@
 import os
 import asyncio
+from pydoc import cli
 from gemmyBot1 import client
 from gemmyTestBot import client2
 from botOneDB import app
@@ -8,18 +9,28 @@ from threading import Thread
 
 
 
+# def Task1():
+#     loop = asyncio.get_event_loop()
+#     loop.create_task(client.start(const.BOT_TOKEN))
+#     loop.create_task(client2.start(const.TEST_BOT_TOKEN))
+#     loop.run_forever()
+
+
 def Task1():
-    loop = asyncio.get_event_loop()
-    loop.create_task(client.start(const.BOT_TOKEN))
-    loop.create_task(client2.start(const.TEST_BOT_TOKEN))
-    loop.run_forever()
+    client.run(const.BOT_TOKEN)
 
 def Task2():
     app.run(debug=False)
 
+def Task3():
+    client.run(const.TEST_BOT_TOKEN)
+
+
 if __name__ == "__main__":
     t1 = Thread(target=Task1)
     t2 = Thread(target=Task2)
+    t3 = Thread(target=Task3)
 
     t1.start()
     t2.start()
+    t3.start()
