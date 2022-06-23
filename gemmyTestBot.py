a01ee8eb3c6f822f78d0cce0384f7fcdb8e4dfe9
DONTKILLME <ashishnegi0511@gmail.com>
2022-06-23 12:56:41 +0530

    added space to gemmy bot 2

--- gemmyTestBot.py
+++ gemmyTestBot.py
@@ -2,7 +2,7 @@ from discord.ext import commands;
 import config.constants as const;
 
 
-client2 = commands.Bot(command_prefix='!gemmy')
+client2 = commands.Bot(command_prefix='!gemmy ')
 
 
 @client2.command()
