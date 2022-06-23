15871b9e9f9788ded6efedfc9b4a478b819b2d5e
DONTKILLME <ashishnegi0511@gmail.com>
2022-06-23 12:50:47 +0530

    test2bot check

--- gemmyTestBot.py
+++ gemmyTestBot.py
@@ -2,4 +2,9 @@ from discord.ext import commands;
 import config.constants as const;
 
 
-client2 = commands.Bot(command_prefix='/')
+client2 = commands.Bot(command_prefix='!gemmy')
+
+
+@client2.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
