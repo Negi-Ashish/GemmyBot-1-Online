115a244b4a4a3fa1aeea589209a90bd0c10fe23a
DONTKILLME <ashishnegi0511@gmail.com>
2022-06-25 02:23:30 +0530

    api call

--- gemmyTestBot.py
+++ gemmyTestBot.py
@@ -1,6 +1,6 @@
 from discord.ext import commands;
 import config.constants as const;
-
+import requests
 
 client2 = commands.Bot(command_prefix='!gemmy ')
 
@@ -8,3 +8,9 @@ client2 = commands.Bot(command_prefix='!gemmy ')
 @client2.command()
 async def test(ctx, arg):
     await ctx.send(arg)
+
+
+@client2.command()
+async def test_api(ctx):
+    api_url = "https://gemmy-bot-one-db.herokuapp.com/test_read"
+    await requests.get(api_url)
