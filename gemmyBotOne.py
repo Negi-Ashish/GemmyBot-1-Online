67cd7ae33f5e9135a43fee7044a88c1e29f3d5d0
DONTKILLME <ashishnegi0511@gmail.com>
2022-06-25 15:56:38 +0530

    embed fixed

--- gemmyBotOne.py
+++ gemmyBotOne.py
@@ -71,9 +71,7 @@ async def balance(ctx):
 async def earn(ctx):
     await open_account(ctx.author)
     earnings_message = await earn_gem(ctx.author.id)
-    print(earnings_message)
-    em = discord.Embed(title = f"{ctx.author.name}'s earnings",color =discord.Color.red())
-    em.add_field(earnings_message)
+    em = discord.Embed(title = f"{ctx.author.name}'s earnings",color =discord.Color.red(),description=earnings_message)
     await ctx.send(embed = em)
 
 
