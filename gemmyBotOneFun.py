3e16c43344ac06bdd8dea745ebbaf3ca950a67c5
DONTKILLME <ashishnegi0511@gmail.com>
2022-06-26 00:35:00 +0530

    request.text check

--- gemmyBotOneFun.py
+++ gemmyBotOneFun.py
@@ -83,7 +83,7 @@ async def deposit_withdraw_gem(userID,amount,wallet_balance,bank_balance,method)
 async def fortune_teller(userID,wallet_balance,bank_balance):
     try:
         await deposit_withdraw_gem(userID,wallet_balance,bank_balance,"deduct")
-        message=requests.get(const.FORTUNE_TELLER)
+        message=requests.get(const.FORTUNE_TELLER).text
         return message
     except:
         pass
