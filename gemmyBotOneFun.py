c22402b6de2cfe8d310e997fb786cb191f6cf1df
DONTKILLME <ashishnegi0511@gmail.com>
2022-06-25 15:33:33 +0530

    debug

--- gemmyBotOneFun.py
+++ gemmyBotOneFun.py
@@ -7,6 +7,7 @@ async def open_account(user):
     if(existance):
         return False
     else:
+        print("Inside else")
         try:
             account_json = {"userId":user.id,"walletBalance":0,"bankBalance":0}
             requests.post(const.ADD_ACCOUNT, json=account_json)
@@ -19,7 +20,6 @@ async def check_existance(userID):
     try:
         api_url = f"""{const.CHECK_ACCOUNT}?userID={userID}"""
         response=requests.get(api_url).json()
-        print(response)
         return response['existance']
     except:
         print("There was a Error in getting existance")
@@ -32,7 +32,7 @@ async def get_balance(userID):
         response=requests.get(api_url).json()
         return response
     except:
-        print("There was a Error in getting existance")
+        print("There was a Error in getting balance")
 
 
 
@@ -42,4 +42,4 @@ async def earn_gem(user):
         response=requests.put(api_url).json()
         return response['message']
     except:
-        print("There was a Error in adding account")
+        print("There was a Error in earn_gem")
