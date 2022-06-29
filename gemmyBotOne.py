8f24d1bb990f435fd7693c9248cfb99791949b7b
DONTKILLME <ashishnegi0511@gmail.com>
2022-06-30 00:38:41 +0530

    amount to int

--- gemmyBotOne.py
+++ gemmyBotOne.py
@@ -75,15 +75,21 @@ async def earn(ctx):
 
 @client.command()
 async def bet(ctx,game_name,amount):
-    balance = await get_balance(ctx.author.id)
-    wallet_amount,bank_amount = balance['wallet_balance'],balance['bank_balance']
-    if(amount>wallet_amount):
-        info_message = f"""Your deposit bet amount cannot be greater than {wallet_amount}"""
-        em = discord.Embed(title = f" STONE | PAPER | SCISSOR ",color = discord.Color.red(),description=info_message)
+    try:
+        balance = await get_balance(ctx.author.id)
+        wallet_amount,bank_amount = balance['wallet_balance'],balance['bank_balance']
+        amount = int(amount)
+        if(amount>wallet_amount):
+            info_message = f"""Your bet amount cannot be greater than {wallet_amount}"""
+            em = discord.Embed(title = f" STONE | PAPER | SCISSOR ",color = discord.Color.red(),description=info_message)
+            await ctx.send(embed = em)
+            return 
+        if game_name=="SPS":
+            await SPS(ctx,client,amount)
+    except:
+        info_message = f"""There is a Error in bet command."""
+        em = discord.Embed(title = f"Info",color = discord.Color.red(),description=info_message)
         await ctx.send(embed = em)
-        return 
-    if game_name=="SPS":
-        await SPS(ctx,client,amount)
 
 
 
@@ -98,7 +104,7 @@ async def deposit(ctx,amount):
             wallet_amount,bank_amount = balance['wallet_balance'],balance['bank_balance']
             if amount>wallet_amount:
                 info_message = f"""Your deposit amount cannot be greater than {wallet_amount}"""
-                em = discord.Embed(title = f"INFO",color = discord.Color.red(),description=info_message)
+                em = discord.Embed(title = f"Info",color = discord.Color.red(),description=info_message)
                 await ctx.send(embed = em)
             else:
                 await deposit_withdraw_gem(ctx.author.id,amount,wallet_amount,bank_amount,"deposit")
@@ -125,7 +131,7 @@ async def withdraw(ctx,amount):
             wallet_amount,bank_amount = balance['wallet_balance'],balance['bank_balance']
             if amount>bank_amount:
                 info_message = f"""Your withdraw amount cannot be greater than {bank_amount}"""
-                em = discord.Embed(title = f"INFO",color = discord.Color.red(),description=info_message)
+                em = discord.Embed(title = f"Info",color = discord.Color.red(),description=info_message)
                 await ctx.send(embed = em)
             else:
                 await deposit_withdraw_gem(ctx.author.id,amount,wallet_amount,bank_amount,"withdraw")
