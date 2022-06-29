4bd4e5ca81eaa754ab9604bb9d66339cd8b257ee
DONTKILLME <ashishnegi0511@gmail.com>
2022-06-30 00:33:56 +0530

    added balance check in bet

--- gemmyBotOne.py
+++ gemmyBotOne.py
@@ -74,9 +74,16 @@ async def earn(ctx):
 
 
 @client.command()
-async def bet(ctx,game_name):
+async def bet(ctx,game_name,amount):
+    balance = await get_balance(ctx.author.id)
+    wallet_amount,bank_amount = balance['wallet_balance'],balance['bank_balance']
+    if(amount>wallet_amount):
+        info_message = f"""Your deposit bet amount cannot be greater than {wallet_amount}"""
+        em = discord.Embed(title = f" STONE | PAPER | SCISSOR ",color = discord.Color.red(),description=info_message)
+        await ctx.send(embed = em)
+        return 
     if game_name=="SPS":
-        await SPS(ctx,client)
+        await SPS(ctx,client,amount)
 
 
 
