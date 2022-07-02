23ae5094eab10627d66382f1b30b1be140c91479
DONTKILLME <ashishnegi0511@gmail.com>
2022-07-02 14:29:47 +0530

    test

--- gemmyBotOneFun.py
+++ gemmyBotOneFun.py
@@ -207,6 +207,7 @@ async def RACE(ctx,amount,wallet_balance,bank_balance):
 
         async def race_results(interaction):
             select.disabled = True
+            await interaction.response.edit_message()
             await interaction.response.send_message(f"""hihihihi {select.values}""")
             
 
