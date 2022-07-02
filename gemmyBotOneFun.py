1c31bee801e277257aa8b8eb5065698d1703be33
DONTKILLME <ashishnegi0511@gmail.com>
2022-07-02 17:27:29 +0530

    test

--- gemmyBotOneFun.py
+++ gemmyBotOneFun.py
@@ -190,32 +190,48 @@ async def RTD(ctx,amount,wallet_balance,bank_balance):
         em = discord.Embed(title = f"Info",color =discord.Color.red(),description=info_message)
         message = await ctx.send(embed=em)
 
+
+class MySelectRace(View):
+    @discord.ui.select(placeholder="Choose your Gemmy!",
+            options=[
+                discord.SelectOption(
+                    label="Gemmy#921",
+                    emoji="<:921:992093550772760647>",
+                    description="Gemmy#921 is a swimmer."
+                    ),
+                discord.SelectOption(
+                    label="Gemmy#1456",
+                    emoji="<:1456:992093539360051281>",
+                    description="Gemmy#1456 is a diver"
+                    ),
+                discord.SelectOption(
+                    label="Gemmy#1669",
+                    emoji="<:1669:992093541742415882>",
+                    description="Gemmy#1669 is smart and fast"
+                    ),
+                discord.SelectOption(
+                    label="Gemmy#2495",
+                    emoji="<:2495:992093547069186078>",
+                    description="Gemmy#2495 is a trickster"
+                    ),
+                discord.SelectOption(
+                    label="Gemmy#4634",
+                    emoji="<:4634:992093544238034975>",
+                    description="Gemmy#4634 smokes Weed"
+                    )
+            ])
+    async def race_results(self,select,interaction):
+        select.disabled = True
+        # await interaction.response.edit_message(view=view)
+        # # await interaction.response.followup.send(f"""hihihihi {select.values}""")
+        # await ctx.send(f"You choosed {select.values}")
+        
+
+
+
 async def RACE(ctx,amount,wallet_balance,bank_balance):
     try:
-        # balance = await get_balance(ctx.author.id)
-        # wallet_amount,bank_amount = balance['wallet_balance'],balance['bank_balance']
-        select = Select(
-            placeholder="Choose your Gemmy!",
-            options=[
-                discord.SelectOption(label="Gemmy#921",emoji="<:921:992093550772760647>",description="Gemmy#921 is a professional athlete"),
-                discord.SelectOption(label="Gemmy#1456",emoji="<:1456:992093539360051281>",description="Gemmy#1456 Never Gives Up"),
-                discord.SelectOption(label="Gemmy#1669",emoji="<:1669:992093541742415882>",description="Gemmy#1669 is smart and fast"),
-                discord.SelectOption(label="Gemmy#2495",emoji="<:2495:992093547069186078>",description="Gemmy#2495 is a trickster"),
-                discord.SelectOption(label="Gemmy#4634",emoji="<:4634:992093544238034975>",description="Gemmy#4634 smokes Weed")
-            ]
-        )
-
-        async def race_results(ctx,interaction):
-            select.disabled = True
-            await interaction.response.edit_message(view=view)
-            # await interaction.response.followup.send(f"""hihihihi {select.values}""")
-            await ctx.send(f"You choosed {select.values}")
-            
-
-        select.callback = race_results
-
-        view = View()
-        view.add_item(ctx,select)
+        view = MySelectRace()
         await ctx.send("Choose Your Gemmy!",view=view)
         return 
 
