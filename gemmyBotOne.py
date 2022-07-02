705c44f02a3f0d33c70c3bd3a44c3669f8018566
DONTKILLME <ashishnegi0511@gmail.com>
2022-07-02 12:54:06 +0530

    removed on_message events

--- gemmyBotOne.py
+++ gemmyBotOne.py
@@ -7,7 +7,7 @@ import json;
 from gemmyBotOneFun import open_account,get_balance,earn_gem,SPS,check_existance,deposit_withdraw_gem,fortune_teller,RTD;
 
 # client = commands.Bot(command_prefix='!gemmy ')
-client = commands.Bot(command_prefix='!gemmy',intents=discord.Intents.all)
+client = commands.Bot(command_prefix='!gemmy',intents=discord.Intents.all())
 # os.chdir("X:\GemmyBot-1-Online\economy")
 
 
@@ -18,31 +18,31 @@ async def on_ready():
     await client.change_presence(status=discord.Status.online,activity=discord.Game("Gemmy Games"))
 
 
-@client.event
-async def on_message(message):
-    if message.author==client.user:
-        if message.content=="Hello! Welcome to GemmyBotClub. We are Glad You came to visit.":
-            await message.add_reaction('\U0001F917')
-        return
-    if message.content.startswith("hello") or message.content.startswith("Hello") or message.content.startswith("Hi") or message.content.startswith("hi"):
-        await message.channel.send ("Hello! Welcome to GemmyBotClub. We are Glad You came to visit.")
-    await client.process_commands(message)
+# @client.event
+# async def on_message(message):
+#     if message.author==client.user:
+#         if message.content=="Hello! Welcome to GemmyBotClub. We are Glad You came to visit.":
+#             await message.add_reaction('\U0001F917')
+#         return
+#     if message.content.startswith("hello") or message.content.startswith("Hello") or message.content.startswith("Hi") or message.content.startswith("hi"):
+#         await message.channel.send ("Hello! Welcome to GemmyBotClub. We are Glad You came to visit.")
+#     await client.process_commands(message)
 
 
-@client.event
-async def on_message_edit(before, after):
-    if after.content =='hi' or after.content == "Hi" or after.content == "Hello" or after.content =="hello":
-        await before.channel.send(
-            "Hello! Welcome to GemmyBotClub. We are Glad You came to visit."
-        )
+# @client.event
+# async def on_message_edit(before, after):
+#     if after.content =='hi' or after.content == "Hi" or after.content == "Hello" or after.content =="hello":
+#         await before.channel.send(
+#             "Hello! Welcome to GemmyBotClub. We are Glad You came to visit."
+#         )
 
-@client.event
-async def on_reaction_add(reaction, user):
-    if user==client.user:
-        return
-    if user == reaction.message.author:
-        print(reaction.message)
-        await reaction.message.channel.send(f"{user} chosed {reaction.emoji}")
+# @client.event
+# async def on_reaction_add(reaction, user):
+#     if user==client.user:
+#         return
+#     if user == reaction.message.author:
+#         print(reaction.message)
+#         await reaction.message.channel.send(f"{user} chosed {reaction.emoji}")
 
 
 # ==================================================================================================================================#
