a87990375b80dea1bdc1cc9e1a0e518bf34801dc
DONTKILLME <ashishnegi0511@gmail.com>
2022-07-03 20:51:32 +0530

    test

--- config/gemmyrace.py
+++ config/gemmyrace.py
@@ -2,9 +2,6 @@ from discord.ui import Select,View;
 import discord;
 
 class MySelectRace(View):
-    def __init__(self,author):
-        self.author = author
-
     @discord.ui.select(placeholder="Choose your Gemmy!",
             options=[
                 discord.SelectOption(
@@ -36,7 +33,7 @@ class MySelectRace(View):
             )
     async def select_callback(self,interaction,select):
         try:
-            print("ctx_user",self.author)
+            # print("ctx_user",self.author)
             print("user",interaction.user)
             select.disabled = True
             await interaction.response.edit_message(view=self)
--- gemmyBotOneFun.py
+++ gemmyBotOneFun.py
@@ -196,7 +196,7 @@ async def RTD(ctx,amount,wallet_balance,bank_balance):
 
 async def RACE(ctx,amount,wallet_balance,bank_balance):
     try:
-        view = MySelectRace(ctx.author)
+        view = MySelectRace()
         await ctx.send("Choose Your Gemmy!",view=view)
 
         return 
