d03df2cb7a7a22e44a73fe0b87912b2af0b56f72
DONTKILLME <ashishnegi0511@gmail.com>
2022-07-06 19:42:50 +0530

    Test display select rce

--- config/gemmyrace.py
+++ config/gemmyrace.py
@@ -48,15 +48,20 @@ class MySelectRace(View):
                 select.disabled = True
                 await interaction.response.edit_message(view=self)
                 info_message = f"""You have selected {select.values[0]}"""
+                em = discord.Embed(title = f"Gemmy Race",color =discord.Color.green(),description=info_message)
                 gemmy_list = ["Gemmy#921","Gemmy#1456","Gemmy#4634","Gemmy#1669","Gemmy#2495"]
                 winner_gemmy = random.randrange(0,4)
                 info_message = " ".join([info_message,f"""\nAnd {gemmy_list[winner_gemmy]} has won the race."""])
                 if select.values[0]!=gemmy_list[winner_gemmy]:
-                    info_message = " ".join([info_message,f"""\nResult : Lose, You lose {self.amount} gems."""])
+                    em.add_field(name="Result",value = "Loose")
+                    em.add_field(name="Gems", value=f" {self.amount * 5} :moneybag:",inline=False)
+                    # info_message = " ".join([info_message,f"""\nResult : Lose, You lose {self.amount} gems."""])
                     info_message = " ".join([info_message,f"""\nWinning a race will grant you five times the gems you bet."""])
                     self.wallet_balance = self.wallet_balance-self.amount
                 else:
-                    info_message = " ".join([info_message,f"""\nResult : Win, Congrulations!! You won {self.amount * 5} gems."""])
+                    em.add_field(name="Result",value = "Win")
+                    # info_message = " ".join([info_message,f"""\nResult : Win, Congrulations!! You won {self.amount * 5} gems."""])
+                    em.add_field(name="Gems", value=f" {self.amount * 5} :moneybag:",inline=False)
                     self.wallet_balance = self.wallet_balance+(self.amount * 5)
                 if(self.wallet_balance<0 or self.bank_balance<0):
                     print("There was a impossible Error in deposit_withdraw_gem")
@@ -64,13 +69,15 @@ class MySelectRace(View):
                 account_json = {"userId":self.ctx.author.id,"walletBalance":self.wallet_balance,"bankBalance":self.bank_balance}
                 headers = {"GEMMY_ACCESS_TOKEN":const.GEMMY_ACCESS_TOKEN,"Content-Type": "application/json; charset=utf-8"}
                 requests.put(const.UPDATE_BALANCE, json=account_json,headers=headers)
-                em = discord.Embed(title = f"Gemmy Race",color =discord.Color.green(),description=info_message)
+                
+                em.description(info_message)
                 em.add_field(name="Wallet Balance",value = self.wallet_balance)
                 em.add_field(name="Bank Balance",value = self.bank_balance)
                 await interaction.followup.send(embed=em)
             else:
                 info_message = f"""Sorry You are not playing this game type '!gemmy bet race amount' to play"""
                 em = discord.Embed(title = f"Info",color =discord.Color.red(),description=info_message)
+                
                 await interaction.response.send_message(embed=em,ephemeral=True)
             # await interaction.response.followup.send(f"""hihihihi {select.values}""")
             # await interaction.response.FollowupMessage(f"You choosed {select.values}")
