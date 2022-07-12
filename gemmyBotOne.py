b4b3c7931d7ca26f3cfd623048c71ba44930b8a5
DONTKILLME <ashishnegi0511@gmail.com>
2022-07-12 17:42:42 +0530

    changed display

--- gemmyBotOne.py
+++ gemmyBotOne.py
@@ -212,24 +212,29 @@ async def info(ctx,command):
                               \nThis command will let you earn random amount of gems within a interval of 4 hours.\n
                               If you encounter an error make sure you have created a account first."""
         elif command=="deposit":
-            info_message="""Syntax:"!gemmy deposit amount"\n amount is the number of gems you want to transfer
+            info_message="""Syntax:"!gemmy deposit amount"
+                            \n amount is the number of gems you want to transfer
                             \nThis command will help you to deposit your gems from your wallet to your bank account.\n
                             You can only transfer amount smaller than equal to your wallet balance."""
         elif command=="withdraw":
-            info_message="""Syntax:"!gemmy withdraw amount"\n amount is the number of gems you want to transfer
+            info_message="""Syntax:"!gemmy withdraw amount"
+                            \n amount is the number of gems you want to transfer
                             \nThis command will help you to withdraw your gems from your bank to your wallet.\n
                             You can only transfer amount smaller than equal to your bank balance."""
         elif command=="bet":
-            info_message= """Syntax:"!gemmy bet game_name amount"\n game name can be
+            info_message= """Syntax:"!gemmy bet game_name amount"
+                             \n game name can be
                              \n"SPS" (STONE PAPER SCISSOR)
                              \n"RACE" (GEMMY RACE)
                              \n"RTD" (ROLL THE DICE)
                              \n amount is the number of gems you want to bet
                             """
         elif command=="fortune":
-            info_message= """Syntax:"!gemmy fortune"\nThis command will tell you your fortune for 50 gems. """
+            info_message= """Syntax:"!gemmy fortune"
+                            \nThis command will tell you your fortune for 50 gems. """
         elif command=="fd":
-            info_message= """Syntax:"!gemmy fd amount"\nThis command will help you start a Fixed Deposit of 3 days.
+            info_message= """Syntax:"!gemmy fd amount"
+                             \nThis command will help you start a Fixed Deposit of 3 days.
                              \nYou cannot make a fixed deposit of amount greater than your bank balance.
                              \nOnce you make a FD the amount is transferred to your FD account and locked for 3 days.
                              \nYou cannot break you FD so think before you start one.
